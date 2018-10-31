20bae2085440e9b8cfe0fc1c56d3c9aabff9953c
zc3212521 <827058891@qq.com>
2018-10-31 16:52:50 +0800

    添加文件loader

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,5 +1,5 @@
 const path = require("path")
-const webpack = require("webpack")
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     output: {
@@ -25,6 +25,53 @@ module.exports = {
                     path.join(__dirname, '../node_modules')
                 ]
             },
+            {
+                test: /\.css$/,
+                use: [
+                    // MiniCssExtractPlugin.loader,
+                    'css-loader?minimize'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    // MiniCssExtractPlugin.loader,
+                    'css-loader?modules&localIdentName=[name]-[local]-[hash:base64:5]&minimize',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    // MiniCssExtractPlugin.loader,
+                    'css-loader?modules&localIdentName=[name]-[local]-[hash:base64:5]&minimize',
+                    'sass-loader',
+                    {
+                        loader: 'sass-resources-loader',
+                        options: {
+                            resources: [
+                                path.resolve(__dirname, '../node_modules/mixins-sass/src/mixins.scss'),
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|png)$/,
+                loader: 'url-loader?limit=8192'
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.xml$/,
+                loader: 'xml-loader'
+            }
         ]
     },
 }
